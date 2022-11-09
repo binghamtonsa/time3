84151ce65adca2abcec0e894a0c630bc4b184c45
Dhruv <dhruv.engineer24@gmail.com>
2022-11-08 19:45:38 -0500

    Update login.js

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import "./App.css";
+import { React, useState } from "react";
 
-export default function Preferences() {
-  return(
-    <h2>Login</h2>
+function Login() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async () => {
+    // API call
+  }
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          value={pass}
+          placeholder="Password"
+          onChange={(e) => setPass(e.target.value)}
+        />
+        <button type="submit">Sign In</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
   );
 }
+
+export default Login;
