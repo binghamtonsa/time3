272760e0ff60bda5d37184ab9a14fee657a2e655
Dhruv <dhruv.engineer24@gmail.com>
2022-11-09 00:33:28 -0500

    created express server, attempted post req

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -1,14 +1,33 @@
 import "./App.css";
 import { React, useState } from "react";
+import axios from 'axios'
 
 function Login() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [message, setMessage] = useState("");
 
-  let handleSubmit = async () => {
-    // API call
-  }
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("localhost:8080/v1/api/login", {
+        method: "POST",
+        body: JSON.stringify({
+          usrnme: email,
+          pswrd: pass,
+        }),
+      });
+      if (res.status === 200) {
+        setEmail("");
+        setPass("");
+        setMessage("Sign in successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="login">
@@ -18,6 +37,7 @@ function Login() {
           value={email}
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
+          
         />
         <input
           type="text"
@@ -25,7 +45,9 @@ function Login() {
           placeholder="Password"
           onChange={(e) => setPass(e.target.value)}
         />
+        <input type="hidden" name="csrf" value="fna-$jfn3-7DW3#!4yh-3jnas-dugwf-hu!@-!Iduad-9adnq"></input>
         <button type="submit">Sign In</button>
+        
 
         <div className="message">{message ? <p>{message}</p> : null}</div>
       </form>
--- server/server.js
+++ server/server.js
@@ -1,4 +1,25 @@
 const Login = require('./auth.js')
+const express = require('express');
 
-var login1 = new Login(process.env.USRNME,process.env.PSWRD, process.env.PORT);
-login1.request(login1.createOptions(), login1.createBody());
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/v1/api/login', function(req, res) {
+    let data = req.body;
+    res.send({
+      'usrnme': usrnme,
+      'pswrd': pswrd,
+    });
+    console.log(data.usrnme + " " + data.pswrd); 
+  });
+
+//   var login1 = new Login(usrnme, pswrd, 8080);
+//   login1.request(login1.createOptions(), login1.createBody());
+  
+  app.listen(port);
+  console.log('Server started at http://localhost:' + port);
+
+// var login1 = new Login(process.env.USRNME,process.env.PSWRD, process.env.PORT);
