9b3d5435102f404c72e860278a87d2510134170f
Dhruv Engineer <72173316+Dengineer24@users.noreply.github.com>
2022-12-15 14:30:42 -0500

    started working on main dashboard

--- client/src/components/Dashboard/dashboard.jsx
+++ client/src/components/Dashboard/dashboard.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react'
+import './App.css'
+import logo from './sa_log.png'
 
-export default function Dashboard() {
+
+
+
+const Dashboard = () => {
+
+  const handleClockIn = (e) => {
+    
+  }
+
+  const handleClockOut = (e) => {
+    
+  }
+
+
   return(
-    <h2>Dashboard</h2>
+    <div className='dashboard-container'>
+      <div className='nav-container'>
+        <div className='nav-wrapper'>
+          {/* <nav className='navbar'></nav> */}
+          <img id="logo" src={logo} alt='logo-main' />
+          <h1 id='header'>Dashboard</h1>
+        </div>
+      </div>
+      <div className='timesheet-container'>
+        <div className='timesheet-wrapper'>
+          <button type='button' className='clock-in' onClick={handleClockIn}>Clock in</button>
+          <button type='button' className='clock-in' onClick={handleClockOut}>Clock out</button>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default Dashboard
